72633e6feda6d7e10239e07ce98ca17e77aabd0e
sammy <aristocrat4gh@gmail.com>
2025-03-03 14:24:10 +0000

    change config batch

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -4,8 +4,7 @@ from enum import Enum
 from pydantic_settings import BaseSettings
 from starlette.config import Config
 
-current_file_dir = os.path.dirname(os.path.realpath(__file__))
-env_path = os.path.join(current_file_dir, "..", "..", ".env")
+env_path = "/code/.env"
 config = Config(env_path)
 
 
